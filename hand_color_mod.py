92e7e44ead1d919fde456527f143b54320907142
drew1981 <94961790+drew1981@users.noreply.github.com>
2023-03-31 16:24:06 +0200

    Update and rename hand.py to hand_color_mod.py

--- hand_color_mod.py
+++ hand_color_mod.py
@@ -6,6 +6,10 @@ mpHands = mp.solutions.hands
 hands = mpHands.Hands()
 mpDraw = mp.solutions.drawing_utils
 
+# Definisci i colori e lo spessore personalizzati per i punti e le linee
+landmark_drawing_spec = mpDraw.DrawingSpec(color=(255, 102, 0), thickness=2, circle_radius=2)  # Arancione
+connection_drawing_spec = mpDraw.DrawingSpec(color=(0, 255, 0), thickness=2)  # Verde
+
 while True:
     success, image = cap.read()
     if success:
@@ -18,15 +22,22 @@ while True:
 
     # checking whether a hand is detected
     if results.multi_hand_landmarks:
-        for handLms in results.multi_hand_landmarks: # working with each hand
+        for handLms in results.multi_hand_landmarks:  # working with each hand
             for id, lm in enumerate(handLms.landmark):
                 h, w, c = image.shape
                 cx, cy = int(lm.x * w), int(lm.y * h)
 
-                if id == 8 :
+                if id == 8:
                     cv2.circle(image, (cx, cy), 10, (255, 0, 255), cv2.FILLED)
 
-            mpDraw.draw_landmarks(image, handLms, mpHands.HAND_CONNECTIONS)
+            # Modifica i colori e lo spessore dei punti e delle linee qui
+            mpDraw.draw_landmarks(
+                image,
+                handLms,
+                mpHands.HAND_CONNECTIONS,
+                landmark_drawing_spec=landmark_drawing_spec,
+                connection_drawing_spec=connection_drawing_spec
+            )
 
     cv2.imshow("Output", image)
     cv2.waitKey(1)
