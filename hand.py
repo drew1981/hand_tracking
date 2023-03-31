afd070b6d7f303997f9fca67364745d645bf5af1
drew1981 <94961790+drew1981@users.noreply.github.com>
2023-03-31 16:54:57 +0200

    Create hand.py

---
+++ hand.py
@@ -0,0 +1,32 @@
+import cv2
+import mediapipe as mp
+
+cap = cv2.VideoCapture(0)
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
+mpDraw = mp.solutions.drawing_utils
+
+while True:
+    success, image = cap.read()
+    if success:
+        # ribalta l'immagine verticalmente
+        image = cv2.flip(image, 1)
+        imageRGB = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        results = hands.process(imageRGB)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # checking whether a hand is detected
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_hand_landmarks: # working with each hand
+            for id, lm in enumerate(handLms.landmark):
+                h, w, c = image.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+
+                if id == 8 :
+                    cv2.circle(image, (cx, cy), 10, (255, 0, 255), cv2.FILLED)
+
+            mpDraw.draw_landmarks(image, handLms, mpHands.HAND_CONNECTIONS)
+
+    cv2.imshow("Output", image)
+    cv2.waitKey(1)
